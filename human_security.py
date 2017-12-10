0c70a598a2f026f607bd7136f4812f960c92d8cd
mdipierro <massimo.dipierro@gmail.com>
2017-12-09 22:26:41 -0600

    fixed typo

--- human_security.py
+++ human_security.py
@@ -8,7 +8,7 @@ from cryptography.hazmat.primitives.asymmetric import rsa, padding
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.serialization import load_pem_private_key
+from cryptography.hazmat.primitives.serialization import load_pem_private_key, load_pem_public_key
 from cryptography.exceptions import InvalidSignature
 from cryptography.fernet import Fernet
 
@@ -49,7 +49,7 @@ class HumanRSA(object):
     def load_private_pem(self, pem):
         self.private_key = load_pem_private_key(pem, password=None, backend=default_backend())
     def load_public_pem(self, pem):
-        self.public_key = load_pem_public_key(pem, password=None, backend=default_backend())
+        self.public_key = load_pem_public_key(pem, backend=default_backend())
     def encrypt(self, data):
         return self.public_key.encrypt(data, self.pad)
     def decrypt(self, data):
