467eafc9b3e375b0109e7fb304c9beba6ca2aa68
Massimo Di Pierro <massimo.dipierro@gmail.com>
2022-08-26 21:10:13 -0700

    version 1.0

--- human_security/__init__.py
+++ human_security/__init__.py
@@ -1,3 +1,104 @@
-from . core import (HumanAES, HumanRSA)
+# Created by Massimo Di Pierro
+# Entirely based on documentation from https://cryptography.io/
+# Hosted at https://github.com/mdipierro/human_security
+# License BSD
+import base64
 
-__version__ = 0.5
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.serialization import (
+    load_pem_private_key,
+    load_pem_public_key,
+)
+from cryptography.exceptions import InvalidSignature
+from cryptography.fernet import Fernet
+
+
+__version__ = "1.0"
+
+
+class HumanAES(object):
+    def __init__(self, key=None):
+        self.key = key
+
+    def generate(self):
+        self.key = Fernet.generate_key()
+
+    def encrypt(self, value):
+        fernet = Fernet(self.key)
+        return fernet.encrypt(value)
+
+    def decrypt(self, value):
+        fernet = Fernet(self.key)
+        return fernet.decrypt(value)
+
+
+class HumanRSA(object):
+    def __init__(self):
+        self.pad = padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA1()),
+            algorithm=hashes.SHA1(),
+            label=None,
+        )
+
+    def generate(self):
+        self.private_key = rsa.generate_private_key(
+            public_exponent=65537, key_size=2048, backend=default_backend()
+        )
+        self.public_key = self.private_key.public_key()
+
+    def private_pem(self):
+        return self.private_key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.PKCS8,
+            encryption_algorithm=serialization.NoEncryption(),
+        ).decode()
+
+    def public_pem(self):
+        return self.public_key.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo,
+        ).decode()
+
+    def load_private_pem(self, pem):
+        self.private_key = load_pem_private_key(
+            pem.encode(), password=None, backend=default_backend()
+        )
+
+    def load_public_pem(self, pem):
+        self.public_key = load_pem_public_key(pem.encode(), backend=default_backend())
+
+    def encrypt(self, data):
+        return self.public_key.encrypt(data, self.pad)
+
+    def decrypt(self, data):
+        return self.private_key.decrypt(data, self.pad)
+
+    def sign(self, data):
+        signature = self.private_key.sign(
+            data,
+            padding.PSS(
+                mgf=padding.MGF1(hashes.SHA256()), salt_length=padding.PSS.MAX_LENGTH
+            ),
+            hashes.SHA256(),
+        )
+        return base64.b16encode(signature).decode()
+
+    def verify(self, data, signature):
+        signature = base64.b16decode(signature.encode())
+        try:
+            self.public_key.verify(
+                signature,
+                data,
+                padding.PSS(
+                    mgf=padding.MGF1(hashes.SHA256()),
+                    salt_length=padding.PSS.MAX_LENGTH,
+                ),
+                hashes.SHA256(),
+            )
+            return True
+        except InvalidSignature:
+            return False
--- setup.py
+++ setup.py
@@ -1,36 +1,33 @@
 """
-Package that contains the ranking algorithms used by the EVote software (@2008-2019)
+Package that contains the ranking algorithms used by the EVote software (@2008-2022)
 """
 import re
 import ast
 from setuptools import setup
 
-_version_re = re.compile(r'__version__\s+=\s+(.*)')
+_version_re = re.compile(r"__version__\s+=\s+(.*)")
 
-with open('human_security/__init__.py', 'r') as f:
-    version = str(ast.literal_eval(_version_re.search(
-        f.read()).group(1)))
+with open("human_security/__init__.py", "r") as f:
+    version = str(ast.literal_eval(_version_re.search(f.read()).group(1)))
 
 setup(
-    name='human_security',
+    name="human_security",
     version=version,
-    url='https://github.com/mdipierro/human_security',
-    license='BSD',
-    author='Massimo Di Pierro',
-    author_email='massimo.dipierro@gmail.com',
-    maintainer='Massimo Di Pierro',
-    maintainer_email='massimo.dipierro@gmail.com',
-    description='Provides easy to use API the RSA and AES',
+    url="https://github.com/mdipierro/human_security",
+    license="BSD",
+    author="Massimo Di Pierro",
+    author_email="massimo.dipierro@gmail.com",
+    maintainer="Massimo Di Pierro",
+    maintainer_email="massimo.dipierro@gmail.com",
+    description="Provides easy to use API the RSA and AES",
     long_description=__doc__,
-    packages=[],
+    packages=["human_security"],
     include_package_data=True,
     zip_safe=False,
-    platforms='any',
+    platforms="any",
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3',
-        'Topic :: Software Development :: Libraries :: Python Modules'
-    ]
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
 )
